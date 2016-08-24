bfcde2868f4fdb2dac6fcaac9cb4845426da301f
Suman Mukherjee <sumam@codeaurora.org>
2016-08-24 10:28:52 +0530

    QCamera2: Provide camera output colorspace to video.
    Set the camera output colorspace in native handle
    instead of indicating video encoder output colorspace.
    709 was configured to video to get 709 colorspace output from
    video. But now video to decide final configuration and camera
    to configure input format which is 601.
    CRs-Fixed: 997590
    Change-Id: Iedf04bc2968ded10c3e536ec6b23dfe246fa4d00

--- QCamera2/HAL/QCameraMem.cpp
+++ QCamera2/HAL/QCameraMem.cpp
@@ -1267,7 +1267,8 @@ QCameraVideoMemory::QCameraVideoMemory(camera_request_memory memory,
 #endif
     mMetaBufCount = 0;
     mBufType = bufType;
-    mUsage = 0;
+    //Set Default color conversion format
+    mUsage = private_handle_t::PRIV_FLAGS_ITU_R_601_FR;
 }
 
 /*===========================================================================
@@ -1305,7 +1306,7 @@ int QCameraVideoMemory::allocate(uint8_t count, size_t size, uint32_t isSecure)
         return rc;
     }
 
-    int usage = mUsage | private_handle_t::PRIV_FLAGS_ITU_R_709;
+    int usage = mUsage | private_handle_t::PRIV_FLAGS_ITU_R_601_FR;
 
     if (mBufType != CAM_STREAM_BUF_TYPE_USERPTR) {
         rc = allocateMeta(count);
@@ -1367,7 +1368,7 @@ int QCameraVideoMemory::allocateMore(uint8_t count, size_t size)
         return rc;
     }
 
-    int usage = mUsage | private_handle_t::PRIV_FLAGS_ITU_R_709;
+    int usage = mUsage | private_handle_t::PRIV_FLAGS_ITU_R_601_FR;
 
     if (mBufType != CAM_STREAM_BUF_TYPE_USERPTR) {
         for (int i = mBufferCount; i < count + mBufferCount; i ++) {
