05b1e1e5cb7010f04c5ca15ebc86dde04f5ae575
Bill Chen <billchen1977@gmail.com>
2017-01-25 13:58:10 +0800

    Fix build with aosp

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -52,17 +52,17 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/common \
         frameworks/native/include/media/hardware \
         frameworks/native/include/media/openmax \
-        hardware/qcom/media/libstagefrighthw \
+        $(TARGET_OUT_HEADERS)/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../mm-image-codec/qexif \
         $(LOCAL_PATH)/../mm-image-codec/qomx_core \
         $(LOCAL_PATH)/util \
-        hardware/qcom/media/mm-core/inc \
+        $(TARGET_OUT_HEADERS)/mm-core/omxcore
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
         frameworks/native/include/media/hardware \
-        hardware/qcom/camera/QCamera2/HAL
+        $(LOCAL_PATH)/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -83,11 +83,11 @@ ifeq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) <= 22 ))" )))
 LOCAL_CFLAGS += -DUSE_L_MR1
 endif
 
+LOCAL_CFLAGS += -DVANILLA_HAL
+
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
-LOCAL_C_INCLUDES += \
-        hardware/qcom/display/libqservice
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
--- QCamera2/HAL/QCameraStateMachine.cpp
+++ QCamera2/HAL/QCameraStateMachine.cpp
@@ -1554,6 +1554,7 @@ int32_t QCameraStateMachine::procEvtPreviewingState(qcamera_sm_evt_enum_t evt,
             m_bPreviewDelayedRestart =
                     (QCAMERA_SM_EVT_DELAYED_RESTART == cmd_payload->arg2);
 
+#ifndef VANILLA_HAL
             if ((CAMERA_CMD_LONGSHOT_ON == cmd_payload->cmd) &&
                     (m_bPreviewNeedsRestart)) {
                 m_parent->stopPreview();
@@ -1571,6 +1572,7 @@ int32_t QCameraStateMachine::procEvtPreviewingState(qcamera_sm_evt_enum_t evt,
                     }
                 }
             }
+#endif
             result.status = rc;
             result.request_api = evt;
             result.result_type = QCAMERA_API_RESULT_TYPE_DEF;
@@ -1581,6 +1583,7 @@ int32_t QCameraStateMachine::procEvtPreviewingState(qcamera_sm_evt_enum_t evt,
         {
             qcamera_sm_evt_command_payload_t *cmd_payload =
                     (qcamera_sm_evt_command_payload_t *)payload;
+#ifndef VANILLA_HAL
             if ((CAMERA_CMD_LONGSHOT_ON == cmd_payload->cmd) &&
                     (m_bPreviewNeedsRestart) &&
                     (m_bPreviewDelayedRestart)) {
@@ -1593,6 +1596,7 @@ int32_t QCameraStateMachine::procEvtPreviewingState(qcamera_sm_evt_enum_t evt,
                     }
                 }
             }
+#endif
             result.status = rc;
             result.request_api = evt;
             result.result_type = QCAMERA_API_RESULT_TYPE_DEF;
