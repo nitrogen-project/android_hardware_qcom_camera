d7f34b34d7f36bcb51bd0f9c56f107f21f9dd185
cb <billchen1977@gmail.com>
2017-02-01 17:23:48 +0800

    QCamera3: Always init capabilities when open

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -609,6 +609,12 @@ int QCamera3HardwareInterface::openCamera()
         return -ENODEV;
     }
 
+    rc = getCapabilities(mCameraId, mCameraHandle);
+    if (rc) {
+        ALOGE("initCapabilities failed.");
+        goto error_exit;
+    }
+
     mCameraOpened = true;
 
     rc = mCameraHandle->ops->register_event_notify(mCameraHandle->camera_handle,
@@ -664,6 +670,11 @@ int QCamera3HardwareInterface::openCamera()
     CDBG_HIGH("mBootToMonoTimestampOffset = %lld", mBootToMonoTimestampOffset);
 
     return NO_ERROR;
+
+error_exit:
+    mCameraHandle->ops->close_camera(mCameraHandle->camera_handle);
+    mCameraHandle = NULL;
+    return rc;
 }
 
 /*===========================================================================
