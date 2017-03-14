ac8ec4ae8da43c996537b204fe1683dc1f08eed8
cb <billchen1977@gmail.com>
2017-03-14 22:14:31 +0800

    Revert "QCamera2: Detect camera capabilities error"
    This reverts commit d8d0c07c143913d1a82a7bf1b53b21ae62da80ba.

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -2210,7 +2210,6 @@ int QCamera2HardwareInterface::initCapabilities(uint32_t cameraId,
     ATRACE_CALL();
     int rc = NO_ERROR;
     QCameraHeapMemory *capabilityHeap = NULL;
-    cam_capability_t *capability;
 
     /* Allocate memory for capability buffer */
     capabilityHeap = new QCameraHeapMemory(QCAMERA_ION_USE_CACHE);
@@ -2219,10 +2218,9 @@ int QCamera2HardwareInterface::initCapabilities(uint32_t cameraId,
         ALOGE("%s: No memory for cappability", __func__);
         goto allocate_failed;
     }
-    capability = (cam_capability_t *)DATA_PTR(capabilityHeap,0);
 
     /* Map memory for capability buffer */
-    memset(capability, 0, sizeof(cam_capability_t));
+    memset(DATA_PTR(capabilityHeap,0), 0, sizeof(cam_capability_t));
 
     cam_buf_map_type_list bufMapList;
     rc = QCameraBufferMaps::makeSingletonBufMapList(
@@ -2247,23 +2245,15 @@ int QCamera2HardwareInterface::initCapabilities(uint32_t cameraId,
         ALOGE("%s: failed to query capability",__func__);
         goto query_failed;
     }
-    if (capability->preview_sizes_tbl_cnt <= 0 ||
-        capability->video_sizes_tbl_cnt <= 0 ||
-        capability->picture_sizes_tbl_cnt <= 0) {
-        ALOGE("%s: bad capability",__func__);
-        rc = UNKNOWN_ERROR;
-        goto query_failed;
-    }
+    gCamCapability[cameraId] =
+            (cam_capability_t *)malloc(sizeof(cam_capability_t));
 
     if (!gCamCapability[cameraId]) {
-        gCamCapability[cameraId] =
-                (cam_capability_t *)malloc(sizeof(cam_capability_t));
-        if (!gCamCapability[cameraId]) {
-            ALOGE("%s: out of memory", __func__);
-            goto query_failed;
-        }
+        ALOGE("%s: out of memory", __func__);
+        goto query_failed;
     }
-    memcpy(gCamCapability[cameraId], capability, sizeof(cam_capability_t));
+    memcpy(gCamCapability[cameraId], DATA_PTR(capabilityHeap,0),
+                                        sizeof(cam_capability_t));
 
     rc = NO_ERROR;
 
