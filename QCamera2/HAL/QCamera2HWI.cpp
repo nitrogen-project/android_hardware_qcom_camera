43deeebd98b51e432ace1bb11aa14d9f8598244b
cb <billchen1977@gmail.com>
2017-03-14 22:13:55 +0800

    Revert "QCamera2: Retry open when error"
    This reverts commit 03d9d11c0143c4d619b62037cf41a5495533369c.

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -1767,13 +1767,7 @@ int QCamera2HardwareInterface::openCamera(struct hw_device_t **hw_device)
     }
     ALOGI("[KPI Perf] %s: E PROFILE_OPEN_CAMERA camera id %d",
         __func__,mCameraId);
-    int retry = 10;
-    do {
-        rc = openCamera();
-        if (rc == NO_ERROR)
-            break;
-        usleep(100 * 1000U);
-    } while (--retry);
+    rc = openCamera();
     if (rc == NO_ERROR){
         *hw_device = &mCameraDevice.common;
         if (m_thermalAdapter.init(this) != 0) {
